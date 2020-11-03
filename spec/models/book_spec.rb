3b20c2fb7c4fe670de48324e95ae2de3ecf7cdeb
LEARN Academy <devjoelmichael@protonmail.com>
2020-11-03 14:26:01 -0800

    started RSpec testing models

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Can a new user be created? Or will an error occar?
+  it "has to be real" do 
+    # We expect that when we create a new user there will not be an error (to_not)
+    expect{ Book.new }.to_not raise_error
+  end 
+
+  # Does the user have an email?
+  it "has a title" do
+    book = Book.new 
+    book.title = "Dune"
+    # Check to make sure that the email is a string
+    expect(book.title).to be_a String
+    # Check to make sure that the email what you expect it to be
+    expect(book.title).to eq "Dune"
+  end
+
+  it "has a author" do
+    book = Book.new 
+    book.author = "Frank Herbert"
+    # Check to make sure that the email is a string
+    expect(book.author).to be_a String
+    # Check to make sure that the email what you expect it to be
+    expect(book.author).to eq "Frank Herbert"
+  end
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # Can a new user be created? Or will an error occar?
+  it "has to be real" do 
+    # We expect that when we create a new user there will not be an error (to_not)
+    expect{ User.new }.to_not raise_error
+  end 
+
+  # Does the user have an email?
+  it "has a email" do
+    user = User.new 
+    user.email = "test@gmail.com"
+    # Check to make sure that the email is a string
+    expect(user.email).to be_a String
+    # Check to make sure that the email what you expect it to be
+    expect(user.email).to eq "test@gmail.com"
+  end
+
+  it "has a password" do
+    user = User.new
+    user.encrypted_password = "test123"
+    expect(user.encrypted_password).to be_a String
+    expect(user.encrypted_password).to eq "test123"
+  end
+
+
+
+
+
+
 end
