036b62414f8827f2c8f3750f6aa96d84e0a27e1f
drewbrad4 <drewbrad4@gmail.com>
2020-11-09 16:55:49 -0800

    removed rentals from Borrow

--- app/javascript/components/Pages/Borrow.js
+++ app/javascript/components/Pages/Borrow.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types"
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap'
 
-const Borrow = ({ books, rentals }) => (
+const Borrow = ({ books }) => (
     <>
         <h3 className="heading">All Books</h3>
         {
