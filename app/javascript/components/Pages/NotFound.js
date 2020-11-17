306d05a3ec689737825f9d3d69cd50b3e7a55ffc
davidkim4 <dkim4016@gmail.com>
2020-11-16 16:13:46 -0800

    not found spacing and text adjusted

--- app/javascript/components/Pages/NotFound.js
+++ app/javascript/components/Pages/NotFound.js
@@ -2,20 +2,18 @@ import React, { Component } from "react"
 
 import noBook from '../../../assets/images/bookNotHere.png'
 
-export default class NotFound extends Component
-{
-    render()
-    {
+export default class NotFound extends Component {
+    render() {
         return (
-            <div className="notFoundPage" style={{ backgroundColor: "#E3F8F2" }}>
-                <h1  class="libraryHeaderText" style={{margin: '25px', paddingTop: "0vw"}}>
-                    Sorry for the inconvience, <br/>
-                    the page you were looking for <br/>
+            <div className="page" style={{ backgroundColor: "#ECFDFF" }}>
+                <h1 class="libraryHeaderText" style={{ margin: '25px', paddingTop: "0vw", color: "#46C2A5" }}>
+                    Sorry for the inconvience, <br />
+                    the page you were looking for <br />
                     could not be found.
                 </h1>
-                    <br/>
+                <br />
                 <div>
-                    <img style={{height: '600px', width: '600px'}} src={noBook} />
+                    <img style={{ height: '350px', width: '500px' }} src={noBook} />
                 </div>
             </div>
         )
