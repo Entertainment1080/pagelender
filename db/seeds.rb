ddeeaf37c23ed8952ee98f902fe364e6e0a078ba
drewbrad4 <drewbrad4@gmail.com>
2020-11-17 23:05:26 -0800

    finished about us page

--- db/seeds.rb
+++ db/seeds.rb
@@ -95,7 +95,7 @@ books = [
     {
         title: "The Handmaid's Tail",
         author: "Margaret Atwood",
-        description: "",
+        description: "Offred is a Handmaid in the Republic of Gilead. She may leave the home of the Commander and his wife once a day to walk to food markets whose signs are now pictures instead of words because women are no longer allowed to read. She must lie on her back once a month and pray that the Commander makes her pregnant, because in an age of declining births, Offred and the other Handmaids are valued only if their ovaries are viable. Offred can remember the years before, when she lived and made love with her husband, Luke; when she played with and protected her daughter; when she had a job, money of her own, and access to knowledge. But all of that is gone now . . .",
         series: "The Handmaid's Tail",
         pages: 314,
         img_url: "https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1578028274l/38447._SY475_.jpg",
